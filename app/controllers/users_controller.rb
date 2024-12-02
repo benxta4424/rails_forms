14938c21497c57df95149d454572d9fa69c9b139
benxta4424 <benxtaben@gmail.com>
2024-12-02 14:31:05 -0500

    correcting the user controller and new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user=User.new
   end
 
-# app/controllers/users_controller.rb
-def create
-  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  def create
+    @user = User.new(user_params)
 
-  if @user.save
-    redirect_to new_user_path
-  else
-    render :new, status: :unprocessable_entity
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
+
+  private
+  def user_params
+    params.require(:users).permit(:name, :password, :email)
+  end
 end
