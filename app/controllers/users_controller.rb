8bdacba4f004a7c55b4354af66063ca0da787c08
benxta4424 <benxtaben@gmail.com>
2024-12-04 18:57:55 +0200

    finishing the editing view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def create
@@ -13,8 +13,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
   def user_params
-    params.require(:users).permit(:name, :password, :email)
+    params.require(:user).permit(:username, :text, :password)
   end
 end
